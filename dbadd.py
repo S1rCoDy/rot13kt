516a01c99d8e5e5659931c576fa41a3855a2192d
S1rCoDy <kalyakin_tima@mail.ru>
2023-10-28 16:28:33 +0500

    slegka izmenil arhivator

--- dbadd.py
+++ dbadd.py
@@ -1,18 +1,18 @@
 import zipfile
 import os
 
-try:
-    myzip = zipfile.ZipFile("archive.tk", "w")
 
-    for folder, subfolders, files in os.walk("E:\\Проги\\auslogics-disk-defrag-setup"):
-        for file in files:
-            myzip.write(
-                os.path.join(folder, file),
-                os.path.relpath(os.path.join(folder, file), "E:\\Проги\\auslogics-disk-defrag-setup"),
-                compress_type=zipfile.ZIP_DEFLATED
-            )
-finally:
-    myzip.close()
+myzip = zipfile.ZipFile("archive.tk", "w")
+
+for folder, subfolders, files in os.walk("E:\\Проги\\auslogics-disk-defrag-setup"):
+    for file in files:
+        myzip.write(
+            os.path.join(folder, file),
+            os.path.relpath(os.path.join(folder, file), "E:\\Проги\\auslogics-disk-defrag-setup"),
+            compress_type=zipfile.ZIP_DEFLATED, compresslevel=9
+        )
+
+myzip.close()
 
 
 """file = zipfile.ZipFile("archive.tk", "w")
