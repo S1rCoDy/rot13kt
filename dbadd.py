d30f46ec5e34903197dde4ff9ff3d7d5a618d286
S1rCoDy <148563516+S1rCoDy@users.noreply.github.com>
2023-11-20 20:52:35 +0500

    progress

--- dbadd.py
+++ dbadd.py
@@ -1,12 +1,13 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
-import sys
 from PyQt6.QtCore import Qt
+import sys
 
 def rot13(text, direction=1):
     result = ''
     for char in text:
         if char.isalpha():
             is_upper = char.isupper()
+            symbols = LATIN_SYMBOLS if char.isascii() else CYRILLIC_SYMBOLS
             index = symbols.find(char.lower())
             
             # Применяем шифр ROT13 в зависимости от направления (шифровка или расшифровка)
@@ -17,9 +18,9 @@ def rot13(text, direction=1):
             result += char
     return result
 
-symbols = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяbdfghijklmnpqrstuvwxyzәғқңөұүАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯBDFGHIJKLMNPQRSTUVWXYZӘҒҚҢӨҰҮ"
-
-
+LATIN_SYMBOLS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+SYMBOLS = LATIN_SYMBOLS + CYRILLIC_SYMBOLS
 
 # Функция для шифровки текста
 def encrypt(text):
@@ -29,106 +30,6 @@ def encrypt(text):
 def decrypt(text):
     return rot13(text, direction=-1)
 
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-
-        self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(320, 500, 161, 41))
-
-        self.pushButton.setObjectName("pushButton")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(130, 10, 111, 21))
-        self.label.setMaximumSize(QtCore.QSize(151, 21))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.label.setFont(font)
-
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(540, 10, 141, 21))
-        self.label_2.setMaximumSize(QtCore.QSize(151, 21))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.label_2.setFont(font)
-        self.label_2.setObjectName("label_2")
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(30, 59, 311, 411))
-        self.lineEdit.setAlignment(Qt.AlignmentFlag.AlignLeading | Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
-        self.lineEdit.setObjectName("lineEdit")
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_2.setGeometry(QtCore.QRect(450, 60, 311, 411))
-        self.lineEdit_2.setAlignment(Qt.AlignmentFlag.AlignLeading | Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
-        self.lineEdit_2.setObjectName("lineEdit_2")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        self.add_functions()
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Rot13 TK"))
-        self.pushButton.setText(_translate("MainWindow", "Запуск"))
-        self.label.setText(_translate("MainWindow", "Шифровка"))
-        self.label_2.setText(_translate("MainWindow", "Разшифровка"))
-
-    def add_functions(self):
-        self.pushButton.clicked.connect(self.on_pushButton_clicked)
-
-    def on_pushButton_clicked(self):
-        input_text = self.lineEdit.text().strip()
-        output_text = self.lineEdit_2.text().strip()
-
-        if input_text and output_text:
-            QtWidgets.QMessageBox.critical(
-                self.pushButton,
-                "Ошибка",
-                "Только одно поле ввода должно быть заполнено.",
-                QtWidgets.QMessageBox.StandardButton.Ok,
-            )
-            return
-
-        if input_text:
-            result = encrypt(input_text)
-            self.lineEdit_2.setText(result)
-        elif output_text:
-            result = decrypt(output_text)
-            self.lineEdit.setText(result)
-
-
-if __name__ == "__main__":from PyQt6 import QtCore, QtGui, QtWidgets
-import sys
-from PyQt6.QtCore import Qt
-
-def rot13(text, direction=1):
-    result = ''
-    for char in text:
-        if char.isalpha():
-            is_upper = char.isupper()
-            index = symbols.find(char.lower())
-            
-            # Применяем шифр ROT13 в зависимости от направления (шифровка или расшифровка)
-            new_index = (index + direction * 13) % len(symbols)
-            
-            result += symbols[new_index].upper() if is_upper else symbols[new_index]
-        else:
-            result += char
-    return result
-
-symbols = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяbdfghijklmnpqrstuvwxyzәғқңөұүАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯBDFGHIJKLMNPQRSTUVWXYZӘҒҚҢӨҰҮ"
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -221,9 +122,4 @@ if __name__ == "__main__":
     MainWindow.show()
     sys.exit(app.exec())
 
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec())
+
